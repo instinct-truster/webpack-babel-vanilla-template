9f4c041f96441cc854c5ad59db81a8f1a3c81cae
Lance Turbes <instinct_truster@protonmail.com>
2023-07-06 11:26:43 -0700

    feat: create container div for app

--- src/create-app.js
+++ src/create-app.js
@@ -1,6 +1,10 @@
 import { createTitle } from "./components/create-title";
 
 export function createApp() {
+  const container = document.createElement("div");
   const titleEl = createTitle();
-  return titleEl;
+  const titleEl2 = createTitle("Title Two!");
+  container.appendChild(titleEl);
+  container.appendChild(titleEl2);
+  return container;
 }
